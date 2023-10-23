3777dd0813f192d9cd664a07fce2358c11d5406c
moshe maman <moshework1985@gmail.com>
2023-10-23 15:38:49 +0300

    1

---
+++ src/app/features/category/services/category.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AddCategoryRequest } from '../models/add-category-reques.model';
+import { Category } from '../models/category.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  constructor(private http: HttpClient) {}
+
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(
+      `${environment.apiBaseUrl}/api/categories`,
+      model
+    );
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(
+      `${environment.apiBaseUrl}/api/categories`
+    );
+  }
+
+  getCategoryById(id: string): Observable<Category> {
+  return  this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
+}
